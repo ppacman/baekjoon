2cf03f6c2f573a01b767be5a1cd802db4dafadee
ppacman <smwfkim2@naver.com>
2024-03-28 13:09:36 +0900

    알고리즘

--- 백준/Class 1/10871. X보다 작은수/X보다 작은수 .js
+++ 백준/Class 1/10871. X보다 작은수/X보다 작은수 .js
@@ -2,5 +2,9 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath, "utf8").toString().split("\n");
 
-const A = input[0];
-const B = input[1];
+const standard = +input[0].split(" ")[1];
+
+const list = input[1].split(" ");
+
+const result = list.filter((number) => number < standard);
+console.log(result.join(" "));
